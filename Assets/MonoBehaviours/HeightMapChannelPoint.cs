690f0c880ac49c0e6a1a061ddeafdab722e91cb9
Steinle13 <61706672+Steinle13@users.noreply.github.com>
2023-05-18 10:45:37 +0200

    This is a small quality of life change. Now the HeightMapChannelPoint are not all the same size so you can click them if they are at the same position.

--- Assets/MonoBehaviours/HeightMapChannelPoint.cs
+++ Assets/MonoBehaviours/HeightMapChannelPoint.cs
@@ -34,6 +34,24 @@ public class HeightMapChannelPoint : MonoBehaviour {
             ResetColors();
         }
 
+        switch (channel)
+        {
+            case 1:
+                this.transform.localScale = Vector3.one * 0.16f;
+                break;
+
+            case 2:
+                this.transform.localScale = new Vector3(0.18f, 0.14f, 0.18f);
+                break;
+
+            case 3:
+                this.transform.localScale = new Vector3(0.20f, 0.12f, 0.20f);
+                break;
+            default:
+                this.transform.localScale = Vector3.one * 0.16f;
+                break;
+
+        }
     }
 
 
@@ -60,7 +78,6 @@ public class HeightMapChannelPoint : MonoBehaviour {
             previousMousePosition = Input.mousePosition;
 
         }
-
     }
 
     public void MoveHeight(float distance) {
