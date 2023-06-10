96a07552f90fccfea98135a8672f38e69e753788
BahKooJ <71286169+BahKooJ@users.noreply.github.com>
2023-06-10 11:52:05 -0600

    Added section edit mode
    -Added Section Edit Mode
    -Removed section tools from Geometry Edit Mode
    -Added controls for quickly selecting edit mode/tool

---
+++ Assets/MonoBehaviours/Views/SectionEditView.cs
@@ -0,0 +1,29 @@
+﻿
+
+using UnityEngine;
+
+public class SectionEditView : MonoBehaviour {
+
+    public SectionEditMode controller;
+
+    public void OnClickCopySectionDataButton() {
+        controller.CopySectionData();
+    }
+
+    public void OnClickPasteSectionDataButton() {
+        controller.PasteSectionData();
+    }
+
+    public void OnClickMirrorSectionVerticallyButton() {
+        controller.MirrorSectionVertically();
+    }
+
+    public void OnClickMirrorSectionHorizontallyButton() {
+        controller.MirrorSectionHorizontally();
+    }
+
+    public void OnClickMirrorSectionDiagonallyButton() {
+        controller.MirrorSectionDiagonally();
+    }
+
+}
