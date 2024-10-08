4d49fc17f25705d6c058b7806d23ae17629b75cc
BahKooJ <71286169+BahKooJ@users.noreply.github.com>
2024-10-08 17:15:57 -0600

    Finished tile split preview

--- Assets/MonoBehaviours/EditorObjects/TileSplitLine.cs
+++ Assets/MonoBehaviours/EditorObjects/TileSplitLine.cs
@@ -16,29 +16,157 @@ public class TileSplitLine : MonoBehaviour {
     private void Start() {
 
         if (topBottom) {
-            line.SetPosition(0, new Vector3(
-                x: tile.column.x,
-                y: tile.column.heights[0].GetPoint(tile.verticies[0].heightChannel),
-                z: tile.column.y));
-
-            line.SetPosition(1, new Vector3(
-                x: tile.column.x + 1,
-                y: tile.column.heights[3].GetPoint(tile.verticies[3].heightChannel),
-                z: tile.column.y + 1));
+            TopBottom();
         }
         else {
-            line.SetPosition(0, new Vector3(
-                x: tile.column.x,
-                y: tile.column.heights[2].GetPoint(tile.verticies[2].heightChannel),
-                z: tile.column.y + 1));
-
-            line.SetPosition(1, new Vector3(
-                x: tile.column.x + 1,
-                y: tile.column.heights[1].GetPoint(tile.verticies[1].heightChannel),
-                z: tile.column.y));
+            BottomTop();
         }
 
 
+
+    }
+
+    void TopBottom() {
+
+        int originalMeshID = (int)MeshType.IDFromVerticies(tile.verticies);
+
+        float x1 = 0f;
+        float y1 = 0f;
+        float z1 = 0f;
+        float x2 = 0f;
+        float y2 = 0f;
+        float z2 = 0f;
+
+        if (MeshType.topWallMeshes.Contains(originalMeshID)) {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[0].GetPoint(tile.verticies[1].heightChannel);
+            z1 = tile.column.y;
+
+            x2 = tile.column.x + 1;
+            y2 = tile.column.heights[1].GetPoint(tile.verticies[2].heightChannel);
+            z2 = tile.column.y;
+
+        }
+        else if (MeshType.leftWallMeshes.Contains(originalMeshID)) {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[0].GetPoint(tile.verticies[0].heightChannel);
+            z1 = tile.column.y;
+
+            x2 = tile.column.x;
+            y2 = tile.column.heights[2].GetPoint(tile.verticies[3].heightChannel);
+            z2 = tile.column.y + 1;
+
+        }
+        else if (MeshType.diagonalTLeftBRightQuadWallMeshes.Contains(originalMeshID)) {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[0].GetPoint(tile.verticies[1].heightChannel);
+            z1 = tile.column.y;
+
+            x2 = tile.column.x + 1;
+            y2 = tile.column.heights[3].GetPoint(tile.verticies[2].heightChannel);
+            z2 = tile.column.y + 1;
+
+        }
+        else if (MeshType.diagonalBLeftTRightQuadWallMeshes.Contains(originalMeshID)) {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[2].GetPoint(tile.verticies[1].heightChannel);
+            z1 = tile.column.y + 1;
+
+            x2 = tile.column.x + 1;
+            y2 = tile.column.heights[1].GetPoint(tile.verticies[2].heightChannel);
+            z2 = tile.column.y;
+
+        }
+        else {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[0].GetPoint(tile.verticies[0].heightChannel);
+            z1 = tile.column.y;
+
+            x2 = tile.column.x + 1;
+            y2 = tile.column.heights[3].GetPoint(tile.verticies[3].heightChannel);
+            z2 = tile.column.y + 1;
+
+        }
+        
+        line.SetPosition(0, new Vector3(x1, y1, z1));
+        line.SetPosition(1, new Vector3(x2, y2, z2));
+
+    }
+
+    void BottomTop() {
+
+        int originalMeshID = (int)MeshType.IDFromVerticies(tile.verticies);
+
+        float x1 = 0f;
+        float y1 = 0f;
+        float z1 = 0f;
+        float x2 = 0f;
+        float y2 = 0f;
+        float z2 = 0f;
+
+        if (MeshType.topWallMeshes.Contains(originalMeshID)) {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[0].GetPoint(tile.verticies[0].heightChannel);
+            z1 = tile.column.y;
+
+            x2 = tile.column.x + 1;
+            y2 = tile.column.heights[1].GetPoint(tile.verticies[3].heightChannel);
+            z2 = tile.column.y;
+
+        }
+        else if (MeshType.leftWallMeshes.Contains(originalMeshID)) {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[0].GetPoint(tile.verticies[1].heightChannel);
+            z1 = tile.column.y;
+
+            x2 = tile.column.x;
+            y2 = tile.column.heights[2].GetPoint(tile.verticies[2].heightChannel);
+            z2 = tile.column.y + 1;
+
+        }
+        else if (MeshType.diagonalTLeftBRightQuadWallMeshes.Contains(originalMeshID)) {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[0].GetPoint(tile.verticies[0].heightChannel);
+            z1 = tile.column.y;
+
+            x2 = tile.column.x + 1;
+            y2 = tile.column.heights[3].GetPoint(tile.verticies[3].heightChannel);
+            z2 = tile.column.y + 1;
+
+        }
+        else if (MeshType.diagonalBLeftTRightQuadWallMeshes.Contains(originalMeshID)) {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[2].GetPoint(tile.verticies[0].heightChannel);
+            z1 = tile.column.y + 1;
+
+            x2 = tile.column.x + 1;
+            y2 = tile.column.heights[1].GetPoint(tile.verticies[3].heightChannel);
+            z2 = tile.column.y;
+
+        }
+        else {
+
+            x1 = tile.column.x;
+            y1 = tile.column.heights[2].GetPoint(tile.verticies[2].heightChannel);
+            z1 = tile.column.y + 1;
+
+            x2 = tile.column.x + 1;
+            y2 = tile.column.heights[1].GetPoint(tile.verticies[1].heightChannel);
+            z2 = tile.column.y;
+
+        }
+
+        line.SetPosition(0, new Vector3(x1, y1, z1));
+        line.SetPosition(1, new Vector3(x2, y2, z2));
 
     }
 
